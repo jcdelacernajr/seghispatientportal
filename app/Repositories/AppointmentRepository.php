4389933c481e4ad4d6cb89a734f30a77c578e07f
Juanito <jcdelacernajr@gmail.com>
2025-11-19 11:39:49 +0800

    Rrefactor appointment class

--- app/Repositories/AppointmentRepository.php
+++ app/Repositories/AppointmentRepository.php
@@ -6,9 +6,36 @@ use App\Models\Appointment;
 
 class AppointmentRepository
 {
+     public function query()
+    {
+        return Appointment::with('patient');
+    }
+
+    public function find($id)
+    {
+        return Appointment::findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return Appointment::create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $appointment = $this->find($id);
+        $appointment->update($data);
+        return $appointment;
+    }
+
+    public function delete($id)
+    {
+        return Appointment::destroy($id);
+    }
+
     public function getUpcommingAppointmentsForUser($user, $limit = 5)
     {
-        $query = Appointment::where('status', 'Pending')
+        $query = Appointment::where('status', 'Confirmed')
             ->with('patient')
             ->orderBy('appointment_date', 'asc');
 
