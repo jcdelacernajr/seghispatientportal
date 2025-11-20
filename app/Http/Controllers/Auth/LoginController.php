b61d63d9b330077cae2ca76c6d51f1c0b0f4241a
Juanito <jcdelacernajr@gmail.com>
2025-11-21 06:51:05 +0800

    Cleaning ...

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
             $request->session()->regenerate();
 
             return redirect()->intended(route('dashboard'))
-                ->with('success', 'Welcome back!');
+                ->with('success', 'Welcome!');
         }
 
         return back()->withErrors([
