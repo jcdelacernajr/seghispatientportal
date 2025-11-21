1d50eb9ac952f5e7fb48df4a63359db43e303e0c
Juanito <jcdelacernajr@gmail.com>
2025-11-21 22:17:21 +0800

    test

--- config/database.php
+++ config/database.php
@@ -59,7 +59,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                // PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // This will allow the connection, but it won’t verify the server certificate, which is less secure.
             ]) : [],
         ],
 
